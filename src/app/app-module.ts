b5f408f699fc2601c38966c3e8b299c2237bdde7
Rohit Kumar Dandwani <rohitdandwani97@gmail.com>
2025-11-16 22:20:32 +0400

    Initial commit with static HTML/CSS layout for header, product list, footer

---
+++ src/app/app-module.ts
@@ -0,0 +1,26 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { ProductList } from './components/product-list/product-list';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Footer,
+    ProductList
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
