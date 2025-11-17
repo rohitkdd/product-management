2ea4e4acd810780801dab6aeb0199530d3306d7d
Rohit Kumar Dandwani <rohitdandwani97@gmail.com>
2025-11-17 22:39:44 +0400

    Integration tests added for the CRUD operations

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductList } from './components/product/product-list/product-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductList },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
