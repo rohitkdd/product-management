c37e857e16668645a7b4844cf4f7523d4b499f3b
Rohit Kumar Dandwani <rohitdandwani97@gmail.com>
2025-11-17 22:44:30 +0400

    Refactoring header component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -12,24 +12,5 @@ export class Header {
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
-
-    const button = document.querySelector('.hamburger') as HTMLElement;
-    const nav = document.querySelector('.nav-links') as HTMLElement;
-
-    if (button) button.classList.toggle('open', this.menuOpen);
-    if (nav) {
-      nav.classList.remove('mobile-open', 'mobile-collapsed');
-      nav.classList.add(this.menuOpen ? 'mobile-open' : 'mobile-collapsed');
-    }
   }
-
-  closeMenu() {
-    this.menuOpen = false;
-    const button = document.querySelector('.hamburger') as HTMLElement;
-    const nav = document.querySelector('.nav-links') as HTMLElement;
-
-    button?.classList.remove('open');
-    nav?.classList.add('mobile-collapsed');
-  }
-
 }
